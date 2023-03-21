f0d62757a93af316510b5d8f47667596353d223c
Luis Muñoz <l.munoz.m@outlook.com>
2023-03-21 19:46:38 -0300

    Create payment model
    Add reference to users model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :payments, class_name: "Payment", foreign_key: "author_id"
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_192331) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_213436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_192331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_payments_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_192331) do
   end
 
   add_foreign_key "groups", "users"
+  add_foreign_key "payments", "users", column: "author_id"
 end
