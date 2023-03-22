3bfd0a50d6126da087b2351f9a660fce7f951d57
Luis Muñoz <l.munoz.m@outlook.com>
2023-03-22 13:31:29 -0300

    Configure views as required.
    Show total transaction for each group on index and list of all transactions in show page.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.where(user_id: current_user.id).includes(:user).reverse
+    @groups = Group.where(user_id: current_user.id).includes(:payments).reverse
   end
 
   # GET /groups/1 or /groups/1.json
