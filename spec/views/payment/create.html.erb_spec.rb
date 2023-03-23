d6fb28104c976c8d59379f384dcde5c8f68fa691
Luis Muñoz <l.munoz.m@outlook.com>
2023-03-23 13:33:28 -0300

    Fix rubocop.

--- spec/views/payment/create.html.erb_spec.rb
+++ spec/views/payment/create.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "payment/create.html.erb", type: :view do
+RSpec.describe 'payment/create.html.erb', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
