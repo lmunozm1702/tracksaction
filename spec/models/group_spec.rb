3707ccbb0eb7122214e25de0a1654068f32b1416
Luis Muñoz <l.munoz.m@outlook.com>
2023-03-31 17:37:06 -0300

    Add test for models

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'belongs to a user' do
+      @user = User.new(email: 'a@a', password: '123456')
+      @group = Group.create(name: 'Doro', user_id: @user.id, icon: 'icon1')
+      expect(@group.user_id).to eq(@user.id)
+    end
+  end
 end
